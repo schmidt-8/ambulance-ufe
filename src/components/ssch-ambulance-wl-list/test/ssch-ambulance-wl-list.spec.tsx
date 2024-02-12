3d3fc29f6e83fcbcb17a94b93a96abc34e0eb67b
Samuel Schmidt <samko.schmidt@azet.sk>
2024-02-12 11:52:14 +0100

    unit tests

--- src/components/ssch-ambulance-wl-list/test/ssch-ambulance-wl-list.spec.tsx
+++ src/components/ssch-ambulance-wl-list/test/ssch-ambulance-wl-list.spec.tsx
@@ -7,12 +7,10 @@ describe('ssch-ambulance-wl-list', () => {
       components: [SschAmbulanceWlList],
       html: `<ssch-ambulance-wl-list></ssch-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <ssch-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ssch-ambulance-wl-list>
-    `);
+    const wlList = page.rootInstance as SschAmbulanceWlList;
+    const expectedPatients = wlList?.waitingPatients?.length
+
+    const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+      expect(items.length).toEqual(expectedPatients);
   });
 });
